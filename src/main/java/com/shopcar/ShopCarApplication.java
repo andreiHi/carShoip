3516a6f129166e271eb2b4fa87e64e02a1803def
andreiHi <andreih1981@gmail.com>
2018-06-28 22:09:39 +0300

    init with spring-boot

---
+++ src/main/java/com/shopcar/ShopCarApplication.java
@@ -0,0 +1,12 @@
+package com.shopcar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopCarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopCarApplication.class, args);
+	}
+}
